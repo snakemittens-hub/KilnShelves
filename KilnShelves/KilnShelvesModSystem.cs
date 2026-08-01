30904172fd761422b8d8eaac73940073fbfc1c33
eb3095 <ebennerit@gmail.com>
2026-07-31 17:35:51 -0700

    Fixed ground storage placement on top of full kiln shelves. Added mod config option to disable shelf heat damage.

--- KilnShelves/KilnShelvesModSystem.cs
+++ KilnShelves/KilnShelvesModSystem.cs
@@ -1,15 +1,20 @@
 ﻿using Vintagestory.API.Common;
+using Vintagestory.API.Datastructures;
 using HarmonyLib;
+using Newtonsoft.Json.Linq;
 
 namespace KilnShelves
 {
     public class KilnShelvesModSystem : ModSystem
     {
+        public static KilnShelvesConfig Config = new();
         private Harmony harmony;
 
-        // Called on server and client
         public override void Start(ICoreAPI api)
         {
+            Config = api.LoadModConfig<KilnShelvesConfig>("kilnshelves.json") ?? new KilnShelvesConfig();
+            api.StoreModConfig(Config, "kilnshelves.json");
+
             api.RegisterBlockClass("BlockKilnShelf", typeof(BlockKilnShelf));
             api.RegisterBlockEntityClass("BEKilnShelf", typeof(BlockEntityKilnShelf));
             api.RegisterCollectibleBehaviorClass("kilnshelves.Offhand", typeof(CollectibleBehaviorOffhand));
@@ -20,11 +25,31 @@ namespace KilnShelves
             api.Logger.Notification("Stackable Kiln Shelves Mod: Started.");
         }
 
+        public override void AssetsFinalize(ICoreAPI api)
+        {
+            if (Config.EnableShelfDamage) return;
+
+            foreach (var block in api.World.Blocks)
+            {
+                if (block?.Code == null) continue;
+                if (block.Code.Domain != "kilnshelves") continue;
+                if (!block.Code.Path.Contains("kilnshelf")) continue;
+
+                if (block.Attributes == null)
+                {
+                    block.Attributes = new JsonObject(JObject.Parse("{\"heatResistance\":1.0}"));
+                }
+                else
+                {
+                    block.Attributes.Token["heatResistance"] = 1.0;
+                }
+            }
+        }
+
         public override void Dispose()
         {
             harmony?.UnpatchAll(Mod.Info.ModID);
             base.Dispose();
         }
-
     }
 }
